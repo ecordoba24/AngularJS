5a7e856c644e4aa731a312d0acb85505d203cf06
Eugenio Cordoba <eugenio@nexorone.com>
2014-08-14 16:37:39 -0500

    Barra de progreso y cantidades
    Barra de progreso y cantidad de tareas realizadas, pendientes y total

--- js/ToDo.js
+++ js/ToDo.js
@@ -10,4 +10,24 @@ function controladorTareas($scope){
 			$scope.textoNuevaTarea = '';
 		}
 	};
+
+	$scope.pendientes = function(){
+		var count = 0;
+		angular.forEach($scope.tareas, function(tarea){
+			count += tarea.hecho ? 0 : 1;
+		});
+		return count;
+	};
+
+	$scope.realizadas = function(){
+		var count = 0;
+		angular.forEach($scope.tareas, function(tarea){
+			count += tarea.hecho ? 1 : 0;
+		});
+		return count;
+	};
+
+	$scope.porcentajeCompletado = function(){
+		return $scope.realizadas()/$scope.tareas.length*100 ;
+	};
 }
