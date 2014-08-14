d789d9d3c4648437cd151ea79f4b58ebe60955ae
Eugenio Cordoba <eugenio@nexorone.com>
2014-08-14 12:01:26 -0500

    Agregar Nueva Tarea

--- js/ToDo.js
+++ js/ToDo.js
@@ -1,6 +1,11 @@
 function controladorTareas($scope){
 	$scope.tareas = [
-	{texto: 'test1', hecho: true },
-	{texto: 'test2', hecho: false }
+	{texto: 'Aprender HTML', hecho: true },
+	{texto: 'Aprender AngularJS', hecho: false }
 	];
+
+	$scope.agregarTarea = function() {
+		$scope.tareas.push({texto: $scope.textoNuevaTarea, hecho: false});
+		$scope.textoNuevaTarea = '';
+	};
 }
