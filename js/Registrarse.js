108d8065818a60c8f75effd45d331853fae4281b
yosua24 <e.cordoba.24@gmail.com>
2014-08-15 23:25:18 -0500

    Formulario de Registro
    Formulario de registro de nuevos usuarios con validaciones

---
+++ js/Registrarse.js
@@ -0,0 +1,56 @@
+var RegistrarseApp = angular.module('RegistrarseApp', []);
+
+function controladorTareas($scope){
+	$scope.tareas = [
+	{texto: 'Aprender HTML', hecho: true },
+	{texto: 'Aprender AngularJS', hecho: false }
+	];
+
+	$scope.agregarTarea = function() {
+		if($scope.textoNuevaTarea){
+			$scope.tareas.push({texto: $scope.textoNuevaTarea, hecho: false});
+			$scope.textoNuevaTarea = '';
+		}
+	};
+
+	$scope.pendientes = function(){
+		var count = 0;
+		angular.forEach($scope.tareas, function(tarea){
+			count += tarea.hecho ? 0 : 1;
+		});
+		return count;
+	};
+
+	$scope.realizadas = function(){
+		var count = 0;
+		angular.forEach($scope.tareas, function(tarea){
+			count += tarea.hecho ? 1 : 0;
+		});
+		return count;
+	};
+
+	$scope.porcentajeCompletado = function(){
+		return $scope.realizadas()/$scope.tareas.length*100 ;
+	};
+
+	$scope.stylePorcentaje = function(){
+		if($scope.porcentajeCompletado() == 100){
+			return "success";
+		}
+
+		if($scope.porcentajeCompletado() <= 25){
+			return "danger";
+		}else{
+			return "warning";
+		}
+	};
+
+	$scope.limpiar = function(){
+		var tareasViejas = $scope.tareas;
+		$scope.tareas = [];
+		angular.forEach(tareasViejas, function(tarea) {
+			if (!tarea.hecho) $scope.tareas.push(tarea);
+		});
+		console.log(tareasViejas);
+	};
+}
